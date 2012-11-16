0f0e6d6b9506e049821d7f7585ed82da0148a665
Kenny Ng <kennyng.zq@gmail.com>
2012-11-16 19:53:09 +0000

    Implemented query functionality for all models. No post for allergies/drugs shelf.

---
+++ ergo/views.py
@@ -0,0 +1,175 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+from django.core.urlresolvers import reverse
+
+from ergo_users.models import UserProfile
+from ergo_contacts.models import Contact
+from ergo_info.models import Provider, Immunization, UserToImmunization, Drug, UserToDrug, Allergy, UserToAllergy
+
+
+def index(request):
+    if request.user.is_authenticated():
+        # for authenticated (logged in) users
+        return render_to_response('index/home.html')
+    else:
+        # for anonymous users
+        return HttpResponseRedirect('/accounts/login/')
+        
+
+def profile_get(request):
+    profile = UserProfile.objects.filter(user=request.user)[0]
+    return render_to_response('users/profile.html', {'profile' : profile}, RequestContext(request))
+
+def contacts_get(request):
+    contacts = Contact.objects.filter(user=request.user)
+    return render_to_response('contacts/contacts.html', {'contacts': contacts}, RequestContext(request))
+
+def provider_get(request):
+    p = Provider.objects.filter(user=request.user)[0]
+    return render_to_response('info/providers.html', {'provider': p}, RequestContext(request))
+
+def vaccines_get(request):
+    records = UserToImmunization.objects.filter(user=request.user)
+    vaccines = []
+    for x in records:
+        shot = {'vaccine_name': x.vaccine.vaccine_name, 'vaccine_date': x.vaccine_date}
+        vaccines.append(shot)
+
+    return render_to_response('info/vaccines.html', {'vaccines': vaccines}, RequestContext(request))
+
+def drugs_otc_get(request):
+    records = UserToDrug.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    drugs = []
+    for x in records:
+        if x.drug.drug_category == 'otc':
+            drug = {'drug_name': x.drug.drug_name, 'drug_img': x.drug.drug_img}
+            drugs.append(drug)
+    shelf = [drugs[i:i+3] for i in range(0, len(drugs), 3)]
+    if (len(drugs) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/drugs-otc.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+
+def drugs_prescription_get(request):
+    records = UserToDrug.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    drugs = []
+    for x in records:
+        if x.drug.drug_category == 'prescription':
+            drug = {'drug_name': x.drug.drug_name, 'drug_img': x.drug.drug_img}
+            drugs.append(drug)
+    shelf = [drugs[i:i+3] for i in range(0, len(drugs), 3)]
+    if (len(drugs) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/drugs-prescription.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+
+def drugs_misc_get(request):
+    records = UserToDrug.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    drugs = []
+    for x in records:
+        if x.drug.drug_category == 'misc':
+            drug = {'drug_name': x.drug.drug_name, 'drug_img': x.drug.drug_img}
+            drugs.append(drug)
+    shelf = [drugs[i:i+3] for i in range(0, len(drugs), 3)]
+    if (len(drugs) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/drugs-misc.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+
+
+def allergies_drug_get(request):
+    records = UserToAllergy.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    allergies = []
+    for x in records:
+        if x.allergy.allergy_category == 'drug':
+            allergy = {'allergy_name': x.allergy.allergy_name, 'allergy_img': x.allergy.allergy_img}
+            allergies.append(allergy)
+    shelf = [allergies[i:i+3] for i in range(0, len(allergies), 3)]
+    if (len(allergies) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/allergies-drug.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+
+    
+def allergies_diet_get(request):
+    records = UserToAllergy.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    allergies = []
+    for x in records:
+        if x.allergy.allergy_category == 'diet':
+            allergy = {'allergy_name': x.allergy.allergy_name, 'allergy_img': x.allergy.allergy_img}
+            allergies.append(allergy)
+    shelf = [allergies[i:i+3] for i in range(0, len(allergies), 3)]
+    if (len(allergies) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/allergies-diet.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+
+def allergies_misc_get(request):
+    records = UserToAllergy.objects.filter(user=request.user)
+    cabinet = {}
+    overflow = 0
+    allergies = []
+    for x in records:
+        if x.allergy.allergy_category == 'misc':
+            allergy = {'allergy_name': x.allergy.allergy_name, 'allergy_img': x.allergy.allergy_img}
+            allergies.append(allergy)
+    shelf = [allergies[i:i+3] for i in range(0, len(allergies), 3)]
+    if (len(allergies) % 3) > 0:
+        partition = len(shelf) - 1
+        cabinet['fullshelf'] = shelf[:partition]
+        overflow_shelf = shelf[partition:]
+        cabinet['overflow'] = overflow_shelf
+        overflow = len(overflow_shelf)
+    else:
+        cabinet['fullshelf'] = shelf
+    extra_shelf = 4 - len(shelf)
+    
+    return render_to_response('info/allergies-misc.html', {'cabinet': cabinet, 'overflow': overflow, 'extra_shelf': extra_shelf}, RequestContext(request))
+    
