3c361afa434cad8c5d6d45c408b7121c701d5ccf
Kenny Ng <kennyng.zq@gmail.com>
2012-11-16 19:58:59 +0000

    Modified settings and urls in ergo dir.

--- ergo/settings.py
+++ ergo/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Kenny Ng', 'zqkng@stanford.edu'),
 )
 
 MANAGERS = ADMINS
@@ -24,7 +24,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -45,18 +45,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/ubuntu/cs147/ergo/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/ubuntu/cs147/ergo/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,6 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/ubuntu/cs147/ergo/static_files',
 )
 
 # List of finder classes that know how to find static files in
@@ -78,7 +79,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '7!b7_=crh69qx@@8y@d(&amp;%nrxhkh6%-ud3q8km5=t3mqvdx-0j'
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -106,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/ubuntu/cs147/ergo/templates',
 )
 
 INSTALLED_APPS = (
@@ -115,8 +117,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'django.contrib.humanize',
+    'registration',
+    'ergo',
+    'ergo_info',
+    'ergo_users',
+    'ergo_contacts',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -149,3 +156,19 @@ LOGGING = {
         },
     }
 }
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window (can be any value)
+EMAIL_USE_TLS = True
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+LOGIN_REDIRECT_URL = '/'
+
+# Override sensitive settings information
+try:
+    from local_settings import *
+except ImportError:
+    pass
+    
+
--- ergo/urls.py
+++ ergo/urls.py
@@ -1,17 +1,32 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from ergo import settings
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ergo.views.home', name='home'),
-    # url(r'^ergo/', include('ergo.foo.urls')),
+    url(r'^$', 'ergo.views.index', name='index'),
+    url('^accounts/', include('registration.backends.default.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^profile/$', 'ergo.views.profile_get', name='profile_display'),
+    url(r'^contacts/$', 'ergo.views.contacts_get', name='contacts_display'),
+    url(r'^providers/$', 'ergo.views.provider_get', name='provider_display'),
+    url(r'^vaccines/$', 'ergo.views.vaccines_get', name='vaccines_display'),
+    url(r'^drugs/otc/$', 'ergo.views.drugs_otc_get', name='drugs_otc_display'),
+    url(r'^drugs/prescription/$', 'ergo.views.drugs_prescription_get', name='drugs_prescription_display'),
+    url(r'^drugs/misc/$', 'ergo.views.drugs_misc_get', name='drugs_misc_display'),
+    url(r'^allergies/drug/$', 'ergo.views.allergies_drug_get', name='allergies_drug_get'),
+    url(r'^allergies/diet/$', 'ergo.views.allergies_diet_get', name='allergies_diet_get'),
+    url(r'^allergies/misc/$', 'ergo.views.allergies_misc_get', name='allergies_misc_get'),
 
+    # url(r'^ergo/', include('ergo.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+        urlpatterns += patterns('',
+            (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+             {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+        )
