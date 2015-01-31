b4f660c0243920104babcfac4fbb1e2e6ab14073
Kenny Ng <kennyng.zq@gmail.com>
2015-01-31 00:11:42 -0600

    Changed S3 bucket name to string instead of env param.

--- ergo/settings.py
+++ ergo/settings.py
@@ -128,7 +128,7 @@ except ImportError:
 
 # Use S3 in production
 if not DEBUG:
-    AWS_S3_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET_NAME', '')
+    AWS_S3_BUCKET_NAME = 'heroku-ergo-assets'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
     # Make collectstatic upload only changed files instead of all files
